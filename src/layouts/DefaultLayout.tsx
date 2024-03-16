de13daeb33406f6a80a31abaa30ded0148cd2fb9
Jonika <katyadyduch@gmail.com>
2024-03-16 23:22:23 +0200

    start header

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import { Header } from "../components/header/Header.tsx";
 
 export const DefaultLayout = () => {
   return (
     <>
+      <Header/>
       <Outlet></Outlet>
     </>
   );
