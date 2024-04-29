6c71b6ca3970763b336157faacfa7bc9ad06169a
Olexandr Husak <uamixerz@gmail.com>
2024-04-29 12:48:41 +0300

    Fix errors for build 1.0

--- src/components/productImages/ProductImages.tsx
+++ src/components/productImages/ProductImages.tsx
@@ -1,63 +1,66 @@
-import { useStyles } from "./ProductImagesStyle.ts";
-import { useEffect, useState } from "react";
-import { APP_ENV } from "../../env";
+import {useStyles} from "./ProductImagesStyle.ts";
+import {useEffect, useState} from "react";
+import {APP_ENV} from "../../env";
 import likeIcon from "../../assets/likeIcon.png";
-import { CheckLikedService, LikeService } from "../../services/favoriteService.ts";
-import { useSelector } from "react-redux";
-import { RootState } from "../../app/store.ts";
+import {CheckLikedService, LikeService} from "../../services/favoriteService.ts";
+import {useSelector} from "react-redux";
+import {RootState} from "../../app/store.ts";
 
 export const ProductImages = ({
-  images,
-  productId,
-}: {
-  images: string[];
-  productId: number | undefined;
+                                  images,
+                                  productId,
+                              }: {
+    images: string[];
+    productId: number | undefined;
 }) => {
-  const classes = useStyles();
-  const [imageSelected, setImageSelected] = useState<string>(images[0]);
-  const userId = useSelector((state: RootState) => state.users.user.id);
-  const [isLiked, setIsLiked] = useState<boolean>(false);
+    const classes = useStyles();
+    const [imageSelected, setImageSelected] = useState<string>(images[0]);
+    const userId = useSelector((state: RootState) => state.users.user.id);
+    const [isLiked, setIsLiked] = useState<boolean>(false);
 
-  const likeClick = async () => {
-    if (productId) await LikeService(productId, userId);
+    const likeClick = async () => {
+        if (productId) await LikeService(productId, userId);
+    };
+
+    useEffect(() => {
+       checkLiked();
+    }, []);
+
+  const checkLiked = async () => {
+    if (productId) {
+      const resp = await CheckLikedService(userId, productId);
+      if (resp)
+        setIsLiked(resp);
+    }
   };
 
-  useEffect(() => {
-    if(productId)
-    {
-      const resp = CheckLikedService(userId,productId);
-      if (resp)
-      setIsLiked(resp);
-    }
-  }, []);
-
-  return (
-    <>
-      <div className={classes.wrapper}>
-        <div className={classes.stackVertical}>
-          {images.map((item, key) => {
-            return (
-              <img
-                onClick={() => {
-                  setImageSelected(item);
-                }}
-                className={classes.images}
-                src={APP_ENV.UPLOADS_URL + item}
-                key={key}
-              />
-            );
-          })}
-        </div>
-        <div className={classes.imageMainWrapper}>
-          <img
-            src={APP_ENV.UPLOADS_URL + imageSelected}
-            className={classes.imageMain}
-          ></img>
-          <button className={classes.likeButton} onClick={likeClick}>
-            <img src={likeIcon} className={classes.likeIcon} />
-          </button>
-        </div>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className={classes.wrapper}>
+                <div className={classes.stackVertical}>
+                    {images.map((item, key) => {
+                        return (
+                            <img
+                                onClick={() => {
+                                    setImageSelected(item);
+                                }}
+                                className={classes.images}
+                                src={APP_ENV.UPLOADS_URL + item}
+                                key={key}
+                            />
+                        );
+                    })}
+                </div>
+                <div className={classes.imageMainWrapper}>
+                    <img
+                        src={APP_ENV.UPLOADS_URL + imageSelected}
+                        className={classes.imageMain}
+                    ></img>
+                    <button className={classes.likeButton} onClick={likeClick}>
+                        <img src={likeIcon} className={classes.likeIcon}/>
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 };
