a9a5e0bce47909b4007641f08f11f91b7bf77b4b
Jonika <katyadyduch@gmail.com>
2024-05-15 22:52:50 +0300

    start checkout page

--- src/pages/default/checkout/CheckoutPageStyle.ts
+++ src/pages/default/checkout/CheckoutPageStyle.ts
@@ -1,3 +1,28 @@
 import { createUseStyles } from "react-jss";
 
-export const useStyles = createUseStyles({});
+export const useStyles = createUseStyles({
+  headerWrapper: {
+    height: "60px",
+    position: "fixed",
+    zIndex: "1000",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+    padding: '14px 65px'
+  },
+  lineHeader: {
+    color: "black",
+    height: "1px",
+    opacity: "1",
+    width: "100%",
+  },
+  headerText: {
+    fontFamily: "Source Serif",
+    fontWeight: "400",
+    fontSize: "18px",
+    lineHeight: "22.55px",
+    color: "black",
+  }
+});
